751928c4e3b8ad2d382c047ce489792906366a1d
jsbeserra <joonassouza55@gmail.com>
2023-10-24 13:37:39 -0300

    refactor: pix-accounts-manager cpf

--- pix-service/pix-accounts-manager/src/domain/value-objects/cpf.ts
+++ pix-service/pix-accounts-manager/src/domain/value-objects/cpf.ts
@@ -1,12 +1,12 @@
-import { InvalidCpf } from '../errors/value-objects'
+import { CpfAllDigitsAreTheSame, CpfInvalidDigit, InvalidCpfLength } from '../errors/value-objects'
 
 export default class Cpf {
 
-	private constructor(readonly value) {}
+	private constructor(readonly value:string) {}
 
 	public static create(value:string): Cpf {
 		const cpf = this.sanitizer(value)
-		if (!Cpf.validate(cpf)) throw new InvalidCpf()
+		this.validate(cpf)
 		return new Cpf(cpf)
 	}
   
@@ -15,16 +15,16 @@ export default class Cpf {
 	}
   
 	private static validate(cpf: string) {
-		if (this.isValidLength(cpf)) return false
-		if (this.allDigitsTheSame(cpf)) return false
+		if (this.isValidLength(cpf)) throw new InvalidCpfLength()
+		if (this.allDigitsTheSame(cpf)) throw new CpfAllDigitsAreTheSame()
 		const digit1 = this.calculateDigit(cpf, 10)
 		const digit2 = this.calculateDigit(cpf, 11)
 		const actualDigit = this.extractCheckDigit(cpf)
 		const calculatedDigit = `${digit1}${digit2}`
-		return actualDigit == calculatedDigit
+		if (this.isValidDigit(actualDigit,calculatedDigit)) throw new CpfInvalidDigit()
 	}
   
-	private static calculateDigit(cpf: string, factor: number) {
+	private static calculateDigit(cpf: string, factor: number):number {
 		let total = 0
 		for (const digit of cpf) {
 			if (factor > 1) total += parseInt(digit) * factor--
@@ -33,20 +33,24 @@ export default class Cpf {
 		return rest < 2 ? 0 : 11 - rest
 	}
   
-	private static sanitizer(cpf: string) {
+	private static sanitizer(cpf: string):string {
 		return cpf.replace(/\D/g, '')
 	}
   
-	private static isValidLength(cpf: string) {
+	private static isValidLength(cpf: string): boolean {
 		return cpf.length !== 11
 	}
   
-	private static allDigitsTheSame(cpf: string) {
+	private static allDigitsTheSame(cpf: string): boolean {
 		return cpf.split('').every((c) => c === cpf[0])
 	}
   
-	private static extractCheckDigit(cpf: string) {
+	private static extractCheckDigit(cpf: string):string {
 		return cpf.substring(cpf.length - 2, cpf.length)
+	}
+
+	private static isValidDigit(actualDigit:string,calculatedDigit:string):boolean{
+		return actualDigit != calculatedDigit
 	}
 }
   
